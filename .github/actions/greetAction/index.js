8a5169a12205ca9005ce17eb9c20913b22f16673
Rakshith-Kotian <67151773+Rakshith-Kotian@users.noreply.github.com>
2024-11-01 20:42:21 +0530

    u

--- .github/actions/greetAction/index.js
+++ .github/actions/greetAction/index.js
@@ -8,3 +8,7 @@ try {
 } catch (error) {
   core.setFailed(`Action failed with error: ${error}`);
 }
+
+
+// const github = require('@actions/github) --> this also used in some custom actions..
+// check all the methods available under core and github library for other uses ...( eg; core.getInput is used above to get input from workflow file.. + core.setFailed is used to fail the workflow)
